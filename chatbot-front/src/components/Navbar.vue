806b94c8f9491dcf4928dcb49c4533eb19580fa7
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2022-08-26 13:30:46 -0300

    Modal de perfil

--- chatbot-front/src/components/Navbar.vue
+++ chatbot-front/src/components/Navbar.vue
@@ -1,30 +1,61 @@
 <template>
-  <n-layout-header bordered class="nav">
-    <n-text tag="div" class="logo" @click="$router.push('/')">
-      <Logo />
-    </n-text>
-    <h2 style="margin: auto 20px auto auto;">
-      Olá, fulano!
-    </h2>
-    <n-popover
-      style="padding: 0;"
-      placement="bottom-end"
-      display-directive="show"
-      trigger="click"
-    >
-      <template #trigger>
-        <n-icon-wrapper :size="24" :border-radius="10" :color="'#000'" style="margin-left: auto">
-          <PersonIcon />
-        </n-icon-wrapper>
-      </template>
-      <div style="overflow: auto; max-height: 79vh">
-        <n-menu
-          :options="menuOptions"
-        />
-      </div>
-    </n-popover>
-  </n-layout-header>
+  <div>
+    <n-layout-header bordered class="nav">
+      <n-text tag="div" class="logo" @click="$router.push('/')">
+        <Logo />
+      </n-text>
+      <h2 style="margin: auto 20px auto auto;">
+        Olá, {{ user.name }}!
+      </h2>
+      <n-popover
+        style="padding: 0;"
+        placement="bottom-end"
+        display-directive="show"
+        trigger="click"
+      >
+        <template #trigger>
+          <n-icon-wrapper :size="24" :border-radius="10" :color="'#000'" style="margin-left: auto">
+            <PersonIcon />
+          </n-icon-wrapper>
+        </template>
+        <div style="overflow: auto; max-height: 79vh">
+          <n-menu
+            :options="menuOptions"
+            :on-update:value="menuUpdated"
+          />
+        </div>
+      </n-popover>
+    </n-layout-header>
+    <n-modal v-model:show="showProfileModal">
+      <n-card
+        style="width: 600px"
+        title="Perfil"
+        :bordered="false"
+        size="huge"
+        role="dialog"
+        aria-modal="true"
+      >
+        <n-descriptions label-placement="left" :columns="1">
+          <n-descriptions-item label="Nome">
+            {{ user.name }}
+          </n-descriptions-item>
+          <n-descriptions-item label="E-mail">
+            {{ user.email }}
+          </n-descriptions-item>
+          <n-descriptions-item label="Usuário">
+            {{ user.username }}
+          </n-descriptions-item>
+          <n-descriptions-item label="Admin">
+            <n-tag :type="user.isAdmin? 'success' : 'error'">
+              {{ user.isAdmin? 'Sim' : 'Não' }}
+            </n-tag>
+          </n-descriptions-item>
+        </n-descriptions>
+      </n-card>
+    </n-modal>
+  </div>
 </template>
+
 <script>
 import { NIcon } from 'naive-ui';
 import { h } from 'vue';
@@ -34,10 +65,22 @@ import {
 } from '@vicons/ionicons5';
 import Logo from './Logo.vue';
 
+const user = {
+  username: 'eduardoferro',
+  name: 'eduardo',
+  email: 'eduardo@mail.com',
+  isAdmin: true,
+};
+
 export default {
   components: {
     PersonIcon,
     Logo,
+  },
+  setup() {
+    return {
+      user,
+    };
   },
   data() {
     function renderIcon(icon) {
@@ -58,7 +101,17 @@ export default {
 
     return {
       menuOptions,
+      showProfileModal: false,
     };
+  },
+  methods: {
+    menuUpdated(key) {
+      if (key === 'edit-profile') {
+        this.showProfileModal = true;
+      } else if (key === 'logout') {
+        alert(key);
+      }
+    },
   },
 };
 </script>
