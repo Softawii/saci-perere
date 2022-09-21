54cc72988bdb5bbcb48ed736c8d110b75f6d19f0
Eduardo Cabral <47820549+FerroEduardo@users.noreply.github.com>
2022-09-21 19:58:56 -0300

    Corrige bug de refresh

--- chatbot-front/src/components/CategoryList.vue
+++ chatbot-front/src/components/CategoryList.vue
@@ -161,8 +161,14 @@ export default {
       });
     },
   },
+  watch: {
+    // eslint-disable-next-line object-shorthand
+    '$route.name'(to, from) {
+      this.$emitter.off('refreshCategories', this.updateCategories);
+    },
+  },
   mounted() {
-    this.$emitter.on('refreshCategories', () => this.updateCategories());
+    this.$emitter.on('refreshCategories', this.updateCategories);
     if (!this.globalStore.data.categories?.length) {
       this.updateCategories();
     }
